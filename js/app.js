335688c1193f18a967816d6b09857adf770cb202
ZhoYev <zholob0213@gmail.com>
2023-04-05 15:06:30 +0000

    added changes for responsive website

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
-if (ScrollTrigger.isTouch !== 1) {
+// if (ScrollTrigger.isTouch !== 1) {
     ScrollSmoother.create({
       wrapper: ".wrapper",
       content: ".content",
@@ -54,6 +54,6 @@ if (ScrollTrigger.isTouch !== 1) {
         }
       );
     });
-}
+// }
 
 
